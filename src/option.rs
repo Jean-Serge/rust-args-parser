23bc6ca085697f6a736946e1e8b5fd575e9e842f
Jean-Serge Monbailly <j.monbailly@orange.fr>
2016-04-26 15:03:36 +0200

    Working on parser => type error at compilation time

--- src/option.rs
+++ src/option.rs
@@ -1,8 +1,8 @@
 struct Option {
-    required: bool,
-    nb_params: u8,
-    names: Vec<String>,
-    description: String 
+    required: bool,         // Is this option required
+    nb_params: u8,          // How many parameters are following this option ?
+    names: Vec<String>,     // Vector of name corresponding to this option
+    description: String     // Short description of this option (to generate help)
 }
 
 struct Arguments {
@@ -17,13 +17,21 @@ impl Arguments {
     fn register_option(&mut self, opt: Option) -> () {
         self.args.push(opt);
     }
+
+    fn parse_args(&self) -> Result<Arguments, &str> {
+        let current_args = std::env::args();
+        let parsed_args = Arguments::new();
+
+        //Ok(parsed_args);
+        std::result::Result::Err("You should not do this.");
+    }
 }
 
 fn main() {
     let mut parser = Arguments::new();
-    let opt = Option{required: true, 
-                     nb_params: 2, 
-                     names: vec![String::from("--hello"), String::from("-h")], 
+    let opt = Option{required: true,
+                     nb_params: 2,
+                     names: vec![String::from("--hello"), String::from("-h")],
                      description: String::from("Un argument pour dire bonjour")
     };
 
